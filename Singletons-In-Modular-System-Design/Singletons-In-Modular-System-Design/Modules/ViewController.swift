2a596377c892705fb764aec9db601bdc1604d0e5
Enes Buğra Yenidünya <bugrayenidunya@gmail.com>
2022-08-07 15:26:42 +0300

    Add project files

---
+++ Singletons-In-Modular-System-Design/Singletons-In-Modular-System-Design/Modules/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Singletons-In-Modular-System-Design
+//
+//  Created by Enes Buğra Yenidünya on 7.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
